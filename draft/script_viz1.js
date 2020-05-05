7eb688793b2b24d72d8bf7c7103f995666c1014d
Sylvain Lugeon <sylvain.lugeon@epfl.ch>
2020-05-05 14:21:53 +0200

    result array, listener on selection lists

--- draft/script_viz1.js
+++ draft/script_viz1.js
@@ -7,6 +7,7 @@ function whenDocumentLoaded(action) {
 	}
 }
 
+// represents an average athlete
 class Athlete {
 	constructor(start_year, end_year, nb_samples, age, height, weight, sport, event) {
 	  this.start_year = start_year;
@@ -20,44 +21,119 @@ class Athlete {
 	}
 }
 
+// represents an olympic result
+class Result {
+	constructor(age, height, weight, sport, event, year) {
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+		this.sport = sport;
+		this.event = event;
+		this.year = year;
+	}
+}
 
-function createDataObject(start_year, end_year, sport, event, svg) {
+// store the results, add unique sports as option to the sports selection list
+function createResultArray(resArray) {
 	d3.csv("../data/athlete_events.csv", function(d) {
+
+		// add eash row to resArray and return the sport feature only
+		resArray.push(new Result(+d["Age"], +d["Height"], +d["Weight"], d.Sport, d.Event, +d["Year"]));
 		return {
-			id : +d.ID,
-			name : d.Name,
-			sex : d.Sex,
-			age : +d["Age"],
-			height : +d["Height"],
-			weight : +d["Weight"],
-			sex : d.Sex,
-			team : d.Team,
-			noc : d.NOC,
-			year: +d["Year"],
-			season: d.Season,
-			city : d.City,
 			sport: d.Sport,
-			event: d.Event,
-			medal: d.Medal
 		};
 	}).then(function(data) {
-		data_f = data.filter(function(d){
-			return d.year >= start_year && d.year <= end_year && d.sport == sport;
+
+		// for each unique sport, create an option value in the sports selection list
+		const sportSel =  d3.select('#sport_selector');
+		sports_unique = d3.map(data, function(d){return d.sport;}).keys()		
+		addSportOptions(sportSel, sports_unique);
+	});
+}
+
+// create an average athlete that represents all athletes from a given sport, event and time window
+function averageAthlete(start_year, end_year, sport, event, resArray){
+
+	data_f = resArray.filter(function(d){
+		sameEvent = event == 'All' ? true : d.event == event;
+		sameSport = d.sport == sport;
+		btwYear = d.year >= start_year && d.year <= end_year;
+		return sameEvent && sameSport && btwYear;
+	});
+
+	const mean_age = d3.mean(data_f, function(d) { return d.age; });
+	const mean_weight = d3.mean(data_f, function(d) { return d.weight; });
+	const mean_height = d3.mean(data_f, function(d) { return d.height; });
+	const nb_samples = data_f.length;
+
+	return new Athlete(start_year, end_year, nb_samples, mean_age, mean_height, mean_weight, sport, event);
+}
+
+// add the given list of sports as options to the given selection list (in d3), remove all previous options
+function addSportOptions(sportSel, sports){
+	sportSel.selectAll('option').remove();
+	sportSel.selectAll('option')
+		.data(['None'].concat(sports)) // add None field 
+		.enter()
+		.append('option')
+		.text(function(d) {
+			return d;
+		  })
+		.attr("value", function(d) {
+			return d;
+		  });
+}
+
+// add the given list of event as options to the given selection list (in d3), remove all previous options
+function updateEventOptions(eventSel, selectedSport){
+	eventSel.selectAll('option').remove();
+	const events = resArray.filter(function(d){
+		return d.sport == selectedSport;
+	});
+	const events_unique = d3.map(events, function(d){return d.event;}).keys();
+	eventSel.selectAll('option')
+		.data(['None', 'All'].concat(events_unique)) // add None and All field
+		.enter()
+		.append('option')
+		.text(function(d) {
+			return d;
+		})
+		.attr("value", function(d) {
+			return d;
 		});
+}
 
-		if(event != 'All'){
-			data_f = data.filter(function(d){
-				return d.event == event;
-			});
-		}
 
-		const mean_age = d3.mean(data_f, function(d) { return d.age; });
-		const mean_weight = d3.mean(data_f, function(d) { return d.weight; });
-		const mean_height = d3.mean(data_f, function(d) { return d.height; });
-		const nb_samples = data_f.length;
+whenDocumentLoaded(() => {
 
-		const ath = new Athlete(start_year, end_year, nb_samples, mean_age, mean_height, mean_weight, sport, event);
-		console.log(ath);
+	// create the results array	
+	resArray = [];
+	createResultArray(resArray);
+
+	const svg = d3.select("#display");
+
+	// find selectors, both in d3 or basic JS selection
+	const sportSelD3 = d3.select('#sport_selector');
+	const sportSel = document.getElementById('sport_selector');
+	const eventSelD3 = d3.select('#event_selector');
+	const eventSel = document.getElementById('event_selector');
+
+
+	// update events selection list given change in sports selection list
+	sportSel.addEventListener("change", () => {
+		const selectedSport = sportSel.value
+		updateEventOptions(eventSelD3, selectedSport);
+	});
+	
+
+	// add svg text that describes the mean athletes in the selected sport and event
+	button = document.getElementById('btn');
+	button.addEventListener('click', () => {
+
+		const sport = sportSel.value;
+		const event = eventSel.value;
+
+		const ath = averageAthlete(1992, 2016, sport, event, resArray)
 
 		d3.select("#sport_disp")
 		.text("Sport : " + ath.sport);
@@ -68,27 +144,16 @@ function createDataObject(start_year, end_year, sport, event, svg) {
 		d3.select("#years_disp")
 		.text("From years : " + ath.start_year + " - " + ath.end_year);
 
+		d3.select("#samples_disp")
+		.text("Number of samples : " + ath.nb_samples);
+
 		d3.select("#age_disp")
 		.text("Mean age : " + ath.age);
 
-	  });
-}
+		d3.select("#height_disp")
+		.text("Mean height : " + ath.height);
 
-whenDocumentLoaded(() => {
-
-	const svg = d3.select("#display").append("svg");
-
-	d3.select("#nRadius").on("input", function() {
-		update(+this.value);
-	  });
-
-	button = document.getElementById('btn');
-	button.addEventListener('click', () => {
-		const sport_sel = document.getElementById('sport_selector');
-		const sport = sport_sel.options[sport_sel.selectedIndex].value;
-		const event_sel = document.getElementById('event_selector')
-		const event = event_sel.options[event_sel.selectedIndex].value;
-		createDataObject(1992, 2016, sport, event, svg)
-
+		d3.select("#weight_disp")
+		.text("Mean weight : " + ath.weight);
 	});
 });
